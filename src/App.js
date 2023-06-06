b8c3f7a47d3ecd3d2a546de6a0f747cc5a18c9e4
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-06-06 18:52:28 +0900

    Awesome CRA and useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [value, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time :)");
+  useEffect(() => {
+    console.log("API Called");
+  }, []);
   return (
     <div>
-      <h1>Welcome back to react!</h1>
+      <h1 className={styles.title}>Welcome back to react!</h1>
+      <span>{value}</span>
+      <button onClick={onClick}>Click Me!</button>
     </div>
   );
 }
