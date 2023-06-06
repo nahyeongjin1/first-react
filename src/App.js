d5cbca1027541f4ac82139a8a5ffa425fcde85fd
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-06-06 19:18:56 +0900

    Amazing 'deps' in useEffect function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [value, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time :)");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("API Called");
+    console.log("Only Once");
   }, []);
+  useEffect(() => {
+    console.log("When 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("When 'value' changes");
+  }, [value]);
+  useEffect(() => {
+    console.log("When 'keyword' or 'value' changes");
+  }, [keyword, value]);
   return (
     <div>
       <h1 className={styles.title}>Welcome back to react!</h1>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <span>{value}</span>
       <button onClick={onClick}>Click Me!</button>
     </div>
