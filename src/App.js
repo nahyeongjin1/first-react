b563c49c48fb4f4093b206029b3c3d7fb8e18110
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-06-11 22:26:36 +0900

    Movie app part 2

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
-import { useEffect, useState } from "react";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovie = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.1&sort_by=year"
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovie();
-  }, []);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.hasOwnProperty("genres")
-                  ? movie.genres.map((genre, index) => (
-                      <li key={index}>{genre}</li>
-                    ))
-                  : null}
-              </ul>
-              <hr />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+function Movie({ movie }) {
+  return (
+    <div>
+      <img src={movie.medium_cover_image} alt={movie.title} />
+      <h2>{movie.title}</h2>
+      <p>{movie.summary}</p>
+      <ul>
+        {movie.hasOwnProperty("genres")
+          ? movie.genres.map((genre, index) => <li key={index}>{genre}</li>)
+          : null}
+      </ul>
+      <hr />
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  movie: PropTypes.object.isRequired,
+};
+
+export default Movie;
