52c8a24ef67c2ead5e2e8df121e1e48d22d11622
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-06-17 19:41:34 +0900

    Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,31 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie({ movie }) {
   return (
-    <div>
-      <img src={movie.medium_cover_image} alt={movie.title} />
-      <h2>
-        <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
-      </h2>
-      <p>{movie.summary}</p>
-      <ul>
-        {movie.hasOwnProperty("genres")
-          ? movie.genres.map((genre, index) => <li key={index}>{genre}</li>)
-          : null}
-      </ul>
+    <div className={styles.movie}>
+      <img
+        src={movie.medium_cover_image}
+        alt={movie.title}
+        className={styles.movie__img}
+      />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{movie.year}</h3>
+        <p>
+          {movie.summary.length > 235
+            ? `${movie.summary.slice(0, 235)}...`
+            : movie.summary}
+        </p>
+        <ul className={styles.movie__genres}>
+          {movie.hasOwnProperty("genres")
+            ? movie.genres.map((genre, index) => <li key={index}>{genre}</li>)
+            : null}
+        </ul>
+      </div>
       <hr />
     </div>
   );
